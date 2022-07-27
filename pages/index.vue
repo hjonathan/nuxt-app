4d326cedc988b1f9a1365ec427cee7f84c889315
henry jordan <jonathan.quispe@processmaker.com>
2022-07-26 20:58:44 -0400

    NUXT APP :: hjonathan

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,8 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        nuxt-github-pages
-      </h1>
-      <h2 class="subtitle">
-        Nuxt Starter for Github Pages
-      </h2>
+      <h1 class="title">nuxt-github-pages-henry jordan</h1>
+      <h2 class="subtitle">Nuxt Starter for Github Pages</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
